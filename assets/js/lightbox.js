c6566ab6fa43110b694bb782d99471dbbb91bade
Steven Kong <stevendjkong@gmail.com>
2017-11-24 14:56:48 +0800

    Add some nav dots for gallery

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -37,6 +37,90 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
-// $('.').each(function(index, element) {
 
-// });
+var startX
+var startZ
+var stopX
+var stopZ
+var withinTime
+var slideNum = 0
+var slides = document.querySelector(".view")
+var numOfSlides = slides.children.length - 1
+
+
+function viewWidth() {
+  if(document.width){
+    return document.width;
+  } else if (window.innerWidth) {
+    return window.innerWidth
+  } else {
+    return document.documentElement.clientWidth
+  }
+}
+function slideTo(index) {
+  var prefixes = ["webkitTransform", "oTransform", "mozTransform", "transform"]
+  prefixes.forEach(function(pre){slides.style[pre] = "translateX(-" + viewWidth() * slideNum + "px)"})
+}
+
+function isIntentional() {
+  minDistance = Math.abs(startX - stopX) > (window.innerWidth / 35)
+  withinTime = Math.abs(startZ - stopZ) < 500
+  if(minDistance && withinTime) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getX(event) {
+  if(event.clientX) {
+    return event.clientX
+  } else {
+    return event.changedTouches[0].pageX
+  }
+}
+
+function saveStart(event) {
+  startX = getX(event)
+  startZ = Date.now()
+}
+
+function saveStop(event) {
+    stopX = getX(event)
+    stopZ = Date.now()
+  if(isIntentional() && stopX > startX) {
+    if(slideNum === 0) {
+      slideNum = numOfSlides
+    } else {
+      slideNum = slideNum - 1
+    }
+    slideTo(slideNum)
+  } else if (isIntentional() && startX > stopX) {
+    if(slideNum === numOfSlides) {
+      slideNum = 0
+    } else {
+      slideNum = slideNum + 1
+    }
+    slideTo(slideNum)
+  }
+}
+
+var slider_container = document.querySelector(".gallery")
+slider_container.addEventListener('touchstart', function(event) {
+  saveStart(event)
+}, false)
+
+slider_container.addEventListener('touchend', function(event) {
+  saveStop(event)
+}, false)
+
+slider_container.addEventListener('mousedown', function(event) {
+  saveStart(event)
+}, false)
+
+slider_container.addEventListener('mouseup', function(event) {
+  saveStop(event)
+}, false)
+slider_container.addEventListener('touchmove', function(event){
+  event.preventDefault()
+})
