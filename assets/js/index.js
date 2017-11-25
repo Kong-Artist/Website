20de5681b3c90fde80340fafc90fa6353e929eb6
Steven Kong <stevendjkong@gmail.com>
2017-11-25 14:22:38 +0800

    Add mail sending + footer message

--- assets/js/index.js
+++ assets/js/index.js
@@ -159,3 +159,30 @@ function test(elID)
   var dest = document.getElementById(elID);
   scrollTo(dest.offsetTop, 500);
 }
+
+function contactForm(){
+	$("#contact-submit").on('click',function() {
+		$contact_form = $('#contact-form');
+		
+		var fields = $contact_form.serialize();
+		
+		$.ajax({
+			type: "POST",
+			url: "assets/php/contact.php",
+			data: fields,
+			dataType: 'json',
+			success: function(response) {
+				
+				if(response.status){
+					$('#contact-form input').val('');
+					$('#contact-form textarea').val('');
+				}
+				
+				$('#response').empty().html(response.html);
+			}
+		});
+		return false;
+	});
+}
+
+contactForm();
