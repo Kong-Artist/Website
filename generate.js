a2b5df9357dd6a5cba97b87665e0592db7acd134
Steven Kong <stevendjkong@gmail.com>
2017-11-25 11:34:13 +0800

    Add some pictures, generate caption

--- generate.js
+++ generate.js
@@ -1,4 +1,4 @@
-const folder = process.argv.slice(2).toString();
+const logFile = './assets/img/photo-log';
 const fs = require('fs');
 
 const thumbsPath = '/assets/img/thumbs/';
@@ -7,13 +7,26 @@ const fullsPath = '/assets/img/full/';
 var htmlString = '<div class="gallery">\n\t';
 
 var fileNames = [];
+var captions = [];
 
-fs.readdir(folder, (err, files) => {
-  files.forEach(file => {
-    if (file.includes('jpg')) {
-      fileNames.push(JSON.stringify(file).replace('"', '').replace('"',''));
-    }
-  });
+// fs.readdir(folder, (err, files) => {
+//   files.forEach(file => {
+//     if (file.includes('jpg')) {
+//       fileNames.push(JSON.stringify(file).replace('"', '').replace('"',''));
+//     }
+//   });
+//   generate();
+// });
+
+fs.readFile(logFile, (err, data) => {
+  if (err) console.log(err);
+  content = data.toString().split(/\r?\n/);
+  // Invoke the next step here however you like
+  for (var el in content) {
+    var tokens = content[el].split(',');
+    fileNames.push(tokens[0].trim());
+    captions.push(tokens[1].trim());
+  }
   generate();
 });
 
@@ -35,7 +48,7 @@ function generate() {
     for (var j = 0; j < 6; j++) {
       if (j + (i * 6) >= fileNames.length) break;
 
-      htmlString += `<div class="col-md-6 col-lg-4">\n\t\t\t\t\t<div class="photo">\n\t\t\t\t\t\t<a href="${fullsPath + fileNames[j + (i*6)]}" data-lightbox="gallery"><img src="${thumbsPath + fileNames[j + (i*6)]}" class="hover-shadow"></a>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t`;
+    htmlString += `<div class="col-md-6 col-lg-4">\n\t\t\t\t\t<div class="photo">\n\t\t\t\t\t\t<a href="${fullsPath + fileNames[j + (i*6)]}.jpg" data-lightbox="gallery" data-title="${fileNames[j + (i*6)]}<p>${captions[j + (i*6)]}</p>"><img src="${thumbsPath + fileNames[j + (i*6)]}.jpg" class="hover-shadow"></a>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t`;
       if (j != 5 && (j + (i * 6) + 1 < fileNames.length)) {
         htmlString += '\t';
       }
